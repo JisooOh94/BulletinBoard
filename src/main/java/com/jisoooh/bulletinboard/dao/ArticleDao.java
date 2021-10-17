7f093d0d4760ac7a3bc84fac9ca82207845e0dde
JisooOh94 <chocokon@gmail.com>
2021-10-18 01:39:20 +0900

    [refactor][#5] 에러 핸들링 및 로깅 로직 추가

--- src/main/java/com/jisoooh/bulletinboard/dao/ArticleDao.java
+++ src/main/java/com/jisoooh/bulletinboard/dao/ArticleDao.java
@@ -1,42 +1,48 @@
 package com.jisoooh.bulletinboard.dao;
 
+import static com.jisoooh.bulletinboard.util.ParsingUtil.parse;
+import static com.jisoooh.bulletinboard.util.RetryUtil.*;
+import static java.time.Duration.*;
+
 import java.security.InvalidParameterException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.dao.DataAccessException;
 import org.springframework.r2dbc.core.DatabaseClient;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jisoooh.bulletinboard.model.Article;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.util.function.Tuple2;
+import reactor.util.retry.RetryBackoffSpec;
 
 public class ArticleDao {
 	private final Logger logger = LoggerFactory.getLogger(this.getClass());
 	private final DatabaseClient databaseClient;
-	private final ObjectMapper objectMapper;
+	private final RetryBackoffSpec retryCondition;
 
-	public ArticleDao(DatabaseClient databaseClient, ObjectMapper objectMapper) {
+	public ArticleDao(DatabaseClient databaseClient) {
 		this.databaseClient = databaseClient;
-		this.objectMapper = objectMapper;
+		retryCondition = getRetryCondition(3, ofSeconds(2), "# ArticleDao error. Query execute failed.", DataAccessException.class);
 	}
 
 	public Mono<Article> get(Mono<Integer> articleNoMono) {
 		return articleNoMono
-				.switchIfEmpty(Mono.error(new InvalidParameterException()))
+				.switchIfEmpty(Mono.error(new InvalidParameterException("# ArticleDao.get error. articleNoMono is empty")))
 				.flatMap(articleNo ->
 						databaseClient
 								.sql("SELECT * FROM article WHERE no = :articleNo")
 								.bind("articleNo", articleNo)
 								.fetch()
 								.one()
-								.map(queryResult -> objectMapper.convertValue(queryResult, Article.class))
-				);
+				).retryWhen(retryCondition)
+				.flatMap(queryResult -> parse(queryResult, Article.class, "# ArticleDao.get error. Query result parsing faile."))
+				.onErrorResume(throwable -> Mono.empty());
 	}
 
 	public Flux<Article> getList(Mono<Tuple2<Long, Integer>> tuple) {
 		return tuple
-				.switchIfEmpty(Mono.error(new InvalidParameterException()))
+				.switchIfEmpty(Mono.error(new InvalidParameterException("# ArticleDao.getList error. tuple is empty")))
 				.flatMapMany(tuple2 ->
 						databaseClient
 								.sql("SELECT * FROM article LIMIT :limit OFFSET :offset")
@@ -44,13 +50,14 @@ public class ArticleDao {
 								.bind("limit", tuple2.getT2())
 								.fetch()
 								.all()
-								.map(queryResult -> objectMapper.convertValue(queryResult, Article.class))
-				);
+				).retryWhen(retryCondition)
+				.flatMap(queryResult -> parse(queryResult, Article.class, "# ArticleDao.getList error. Query result parsing failed."))
+				.onErrorResume(throwable -> Mono.empty());
 	}
 
 	public Mono<Void> add(Mono<Article> articleMono) {
 		return articleMono
-				.switchIfEmpty(Mono.error(new InvalidParameterException()))
+				.switchIfEmpty(Mono.error(new InvalidParameterException("# ArticleDao.add error. articleMono is empty")))
 				.flatMap(article ->
 						databaseClient
 								.sql("INSERT INTO article (title, content, registYmdt, modifyYmdt) VALUES (:title, :content, :registYmdt, :modifyYmdt)")
@@ -60,8 +67,10 @@ public class ArticleDao {
 								.bind("modifyYmdt", article.getModifyYmdt())
 								.fetch()
 								.rowsUpdated()
-								.then()
-				);
+				).retryWhen(retryCondition)
+				.doOnError(throwable -> logger.error("# ArticleDao.add error.", throwable))
+				.onErrorResume(throwable -> Mono.empty())
+				.as(queryResult -> queryResult.then());
 	}
 
 	public Mono<Void> update(Mono<Article> articleMono) {
@@ -76,8 +85,10 @@ public class ArticleDao {
 								.bind("articleNo", article.getNo())
 								.fetch()
 								.rowsUpdated()
-								.then()
-				);
+				).retryWhen(retryCondition)
+				.doOnError(throwable -> logger.error("# ArticleDao.update error.", throwable))
+				.onErrorResume(throwable -> Mono.empty())
+				.as(queryResult -> queryResult.then());
 	}
 
 	public Mono<Void> delete(Mono<Integer> articleNoMono) {
@@ -90,6 +101,9 @@ public class ArticleDao {
 								.fetch()
 								.rowsUpdated()
 								.then()
-				);
+				).retryWhen(retryCondition)
+				.doOnError(throwable -> logger.error("# ArticleDao.delete error.", throwable))
+				.onErrorResume(throwable -> Mono.empty())
+				.as(queryResult -> queryResult.then());
 	}
 }
---
+++ src/main/java/com/jisoooh/bulletinboard/util/RetryUtil.java
@@ -0,0 +1,32 @@
+package com.jisoooh.bulletinboard.util;
+
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.time.Duration;
+import java.util.stream.Stream;
+
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import reactor.util.retry.Retry;
+import reactor.util.retry.RetryBackoffSpec;
+
+public class RetryUtil {
+	private static Logger logger = LoggerFactory.getLogger(RetryUtil.class);
+
+	private RetryUtil() {
+		throw new AssertionError();
+	}
+
+	public static final RetryBackoffSpec CommonRetryCondition = getRetryCondition(3, Duration.ofSeconds(2), StringUtils.EMPTY, IOException.class);
+
+	public static RetryBackoffSpec getRetryCondition(int maxAttepts, Duration delay, String retryFailMessage, Class<? extends Throwable>... targetExceptions) {
+		return Retry.backoff(maxAttepts, delay)
+				.filter(throwable -> Stream.of(targetExceptions).anyMatch(exception -> exception.isInstance(throwable)))
+				.doBeforeRetry(retrySignal -> logger.info("# execute Retry"))
+				.onRetryExhaustedThrow(((retryBackoffSpec, retrySignal) -> {
+					logger.error(retryFailMessage, retrySignal.failure());
+					return new UncheckedIOException(retryFailMessage, new IOException(retrySignal.failure()));
+				}));
+	}
+}
